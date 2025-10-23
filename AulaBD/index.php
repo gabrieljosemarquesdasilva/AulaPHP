ead6e7101a0b9568fa6d84fbf02b7bbeae798330
gabrieljosemarquesdasilva <gabrieljosemarquesdasilva@gmail.com>
2025-10-23 12:26:44 -0300

    23/10

---
+++ AulaBD/index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="estilos.css">
+</head>
+<body>
+    
+     <?php include "menu.php" ?>
+
+    <div class="conteudo">
+        <h3>Integração do PHP com o MySQL</h3>
+
+        <p>
+            Nesta aula iremos estudar como integrar/conectar o PHP com o MySQL.
+        </p>
+        <p>
+            O objetivo desse projeto será cadastrar alunos de uma escola.
+        </p>
+    </div>
+</body>
+</html>
