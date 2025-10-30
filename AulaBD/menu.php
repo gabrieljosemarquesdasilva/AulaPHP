c050bd0917265856d10cb37f835aad3613895b7b
Etec <usuario@etec.com>
2025-10-30 12:04:05 -0300

    30/11

--- AulaBD/menu.php
+++ AulaBD/menu.php
@@ -6,5 +6,7 @@
          <div>
             <a href="aluno_cadastro.php">Cadastrar</a>
             <a href="aluno_consulta.php">Consultar</a>
+            <a href="professor_cadastro.php">Prof.Cadastrar</a>
+            <a href="professor_consulta.php">Prof.Consultar</a>-
         </div>
     </div>
--- AulaBD/professor_cadastro.php
+++ AulaBD/professor_cadastro.php
@@ -4,15 +4,32 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Professor Cadastro</title>
+    <link rel="stylesheet" href="estilos.css">
 </head>
 <body>
-    <h1> Cadastro de Professores </h1>
-    <form name="Enviar" method="POST" action="professor_cadastro.php">
-        <fieldset>
-            <p>Informe o nome do professor:</p>
-            <input type="text" name="professor">
 
-            <br>
-            <br>
-            <input type="submit" name="Enviar" value="Cadastrar Professor"> 
-        </fieldset>
+<?php include "menu.php" ?>
+   <div class="conteudo">
+
+    <h3> Cadastro de Professores </h3>
+    <form method="post" action="#">
+
+            <p>Informe o nome<br>
+            <input type="text" name="nome">
+            </p>
+            <p>Informe a area de formação:<br>
+            <input type="text" name="cidade">
+            </p>
+
+            <p>Digite a graduação:<br>
+            <input type="text" name="estado">
+            </p>
+            <p>
+
+            <input type="submit" name="enviar" value="Cadastrar"> 
+            </p>
+    </form>
+</div>
+
+<body>
+    <html>
